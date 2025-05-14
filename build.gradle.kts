4a3bdf4f666ce685f39704f13af74e0c40ef28ce
Semyon Proshev <semyon.proshev@jetbrains.com>
2025-05-14 20:16:35 +0200

    Remove usage of `USER_STGN_JWT_TOKEN`

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,8 +68,6 @@ subprojects {
         }
         environment.putAll(
             mapOf(
-                "USER_STGN_JWT_TOKEN" to
-                        (project.properties["grazieUserStgnToken"] ?: System.getenv("USER_STGN_JWT_TOKEN")),
                 "ANTHROPIC_API_TEST_KEY" to System.getenv("ANTHROPIC_API_TEST_KEY"),
                 "OPEN_AI_API_TEST_KEY" to System.getenv("OPEN_AI_API_TEST_KEY"),
             )
