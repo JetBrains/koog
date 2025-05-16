38e8062699277dc79bc1bcf98aa423ee00afbdf4
Mark.Tkachenko <mark.tkachenko@jetbrains.com>
2025-05-16 17:34:33 +0200

    Ollama client publish fix - 2

--- prompt/prompt-executor/prompt-executor-clients/prompt-executor-ollama-client/build.gradle.kts
+++ prompt/prompt-executor/prompt-executor-clients/prompt-executor-ollama-client/build.gradle.kts
@@ -1,8 +1,5 @@
 import ai.grazie.gradle.publish.maven.Publishing.publishToGraziePublicMaven
 
-group = "${rootProject.group}.prompt"
-version = rootProject.version
-
 plugins {
     id("ai.kotlin.multiplatform")
     alias(libs.plugins.kotlin.serialization)
