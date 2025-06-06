e20c2da0b83de66053c27c354315f86befdf9ea7
Anastasiia.Zarechneva <Anastasiia.Zarechneva@jetbrains.com>
2025-06-06 18:00:44 +0200

    Fix `Retry` annotation

--- integration-tests/src/jvmTest/kotlin/ai/koog/integration/tests/utils/annotations/Retry.kt
+++ integration-tests/src/jvmTest/kotlin/ai/koog/integration/tests/utils/annotations/Retry.kt
@@ -1,5 +1,10 @@
 package ai.koog.integration.tests.utils.annotations
 
+import org.junit.jupiter.api.TestTemplate
+import org.junit.jupiter.api.extension.ExtendWith
+
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
+@TestTemplate
+@ExtendWith(RetryExtension::class)
 annotation class Retry(val times: Int = 3)
