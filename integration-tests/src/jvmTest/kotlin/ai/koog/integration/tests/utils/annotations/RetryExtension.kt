e4cd3545636c22fab41ea9007753f9b174a1a485
Anastasiia.Zarechneva <Anastasiia.Zarechneva@jetbrains.com>
2025-06-06 18:01:00 +0200

    Fix `Retry` annotation

--- integration-tests/src/jvmTest/kotlin/ai/koog/integration/tests/utils/annotations/RetryExtension.kt
+++ integration-tests/src/jvmTest/kotlin/ai/koog/integration/tests/utils/annotations/RetryExtension.kt
@@ -1,10 +1,10 @@
 package ai.koog.integration.tests.utils.annotations
 
 import org.junit.jupiter.api.Assumptions.assumeTrue
-import org.junit.jupiter.api.extension.ExtensionContext
-import org.junit.jupiter.api.extension.TestExecutionExceptionHandler
+import org.junit.jupiter.api.extension.*
+import java.util.stream.Stream
 
-class RetryExtension : TestExecutionExceptionHandler {
+class RetryExtension : TestTemplateInvocationContextProvider, TestExecutionExceptionHandler {
     companion object {
         private val NAMESPACE = ExtensionContext.Namespace.create(RetryExtension::class.java)
 
@@ -23,6 +23,28 @@ class RetryExtension : TestExecutionExceptionHandler {
                 || e.message?.contains(ANTHROPIC_502_ERROR) == true
     }
 
+    override fun supportsTestTemplate(context: ExtensionContext): Boolean {
+        return context.requiredTestMethod.isAnnotationPresent(Retry::class.java)
+    }
+
+    override fun provideTestTemplateInvocationContexts(context: ExtensionContext): Stream<TestTemplateInvocationContext> {
+        val retry = context.requiredTestMethod.getAnnotation(Retry::class.java)
+        return Stream.generate { createInvocationContext(context, retry) }
+            .limit(retry.times.toLong())
+    }
+
+    private fun createInvocationContext(context: ExtensionContext, retry: Retry): TestTemplateInvocationContext {
+        return object : TestTemplateInvocationContext {
+            override fun getDisplayName(invocationIndex: Int): String {
+                return "${context.displayName} [attempt ${invocationIndex}]"
+            }
+
+            override fun getAdditionalExtensions(): List<Extension> {
+                return listOf(RetryExecutionExtension(retry))
+            }
+        }
+    }
+
     override fun handleTestExecutionException(
         context: ExtensionContext,
         throwable: Throwable
@@ -33,8 +55,14 @@ class RetryExtension : TestExecutionExceptionHandler {
             return
         }
 
-        val retry = context.requiredTestMethod.getAnnotation(Retry::class.java)
-        if (retry != null) {
+        throw throwable
+    }
+
+    private class RetryExecutionExtension(private val retry: Retry) : TestExecutionExceptionHandler {
+        override fun handleTestExecutionException(
+            context: ExtensionContext,
+            throwable: Throwable
+        ) {
             val retryStore = context.getStore(NAMESPACE)
             val key = "${context.requiredTestClass.name}.${context.requiredTestMethod.name}"
             val currentAttempt = retryStore.getOrComputeIfAbsent(key, { 0 }, Int::class.java) as Int
@@ -44,11 +72,11 @@ class RetryExtension : TestExecutionExceptionHandler {
             if (currentAttempt < retry.times - 1) {
                 retryStore.put(key, currentAttempt + 1)
                 println("[DEBUG_LOG] Retrying test '${context.displayName}'")
-                return // Don't throw the exception to allow retry
+                return
             } else {
                 println("[DEBUG_LOG] Maximum retry attempts (${retry.times}) reached for test '${context.displayName}'")
+                throw throwable
             }
         }
-        throw throwable
     }
 }
