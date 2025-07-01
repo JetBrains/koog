eb58b485bb2889d5d2e471e36f833ad405c1bc86
devcrocod <devcrocod@gmail.com>
2025-07-01 15:48:29 +0200

    fix publishing spring module

--- koog-spring-boot-starter/build.gradle.kts
+++ koog-spring-boot-starter/build.gradle.kts
@@ -1,10 +1,11 @@
-import ai.grazie.gradle.publish.maven.Publishing.publishToMaven
+import ai.koog.gradle.publish.maven.Publishing.publishToMaven
 
 group = rootProject.group
 version = rootProject.version
 
 plugins {
     id("ai.kotlin.jvm")
+    id("ai.kotlin.jvm.publish")
     alias(libs.plugins.kotlin.spring)
     alias(libs.plugins.spring.management)
 }
@@ -16,7 +17,7 @@ kotlin {
 dependencies {
     api(project(":koog-agents"))
 
-    implementation(project.dependencies.enforcedPlatform(libs.spring.boot.bom))
+    implementation(project.dependencies.platform(libs.spring.boot.bom))
     api(libs.bundles.spring.boot.core)
     api(libs.reactor.kotlin.extensions)
 
