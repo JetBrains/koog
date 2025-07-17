fcede09169237d51befa88ccd757c84249dea715
Sergey Karpov <sergey.karpov@jetbrains.com>
2025-07-17 14:16:00 +0300

    fixup! FileSystemProvider specification
    Signed-off-by: Sergey Karpov <sergey.karpov@jetbrains.com>

--- rag/rag-base/src/jvmMain/kotlin/ai/koog/rag/base/files/JVMFileSystemProvider.kt
+++ rag/rag-base/src/jvmMain/kotlin/ai/koog/rag/base/files/JVMFileSystemProvider.kt
@@ -14,6 +14,7 @@ import java.nio.charset.Charset
 import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.io.NoSuchFileException
 import kotlin.io.path.*
 import kotlin.use
 
@@ -447,6 +448,7 @@ public object JVMFileSystemProvider {
          *
          * @param parent The parent path in which the file or directory resides.
          * @param name The name of the file or directory to be deleted.
+         * @throws NoSuchFileException if a file or directory doesn't exist.
          */
         @OptIn(ExperimentalPathApi::class)
         override suspend fun delete(parent: Path, name: String) {
