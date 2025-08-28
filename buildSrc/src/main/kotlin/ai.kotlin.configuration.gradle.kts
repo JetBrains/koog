c38b4831eeefdd4939517f1a9f39143736e5c63d
Andrey Bragin <andrey.bragin@jetbrains.com>
2025-08-28 17:00:52 +0200

    Improve Kotlin version management in convention gradle plugin

--- buildSrc/src/main/kotlin/ai.kotlin.configuration.gradle.kts
+++ buildSrc/src/main/kotlin/ai.kotlin.configuration.gradle.kts
@@ -1,12 +1,20 @@
+import org.gradle.accessors.dm.LibrariesForLibs
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
+val libs = the<LibrariesForLibs>()
+
+val kotlinLanguageVersion = KotlinVersion.KOTLIN_2_1
+val kotlinApiVersion = KotlinVersion.KOTLIN_2_1
+val kotlinCoreLibrariesVersion = "2.1.0"
+
 extensions.getByType<KotlinProjectExtension>().apply {
     jvmToolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
+
     sourceSets.all {
         languageSettings {
             // K/Common
@@ -16,11 +24,21 @@ extensions.getByType<KotlinProjectExtension>().apply {
             // K/JS
             optIn("kotlin.js.ExperimentalJsExport")
         }
+
+        /*
+         Advise the correct version of kotlin stdlib and core libraries.
+         This does not fix the problem when some library brings the higher version of Kotlin transitively.
+         We can't use enforcedPlatform() with implementation, because it would leak this Kotlin version constraint to the consumers.
+         We can't use enforcedPlatform() with compileOnly either, because not all KMP targets support compileOnly.
+         That's why below we configure resolutionStrategy manually, to pick the right version of Kotlin.
+         */
+        dependencies {
+            implementation(project.dependencies.platform(libs.kotlin.bom))
+        }
+
+        coreLibrariesVersion = kotlinCoreLibrariesVersion
     }
 }
-
-val kotlinLanguageVersion = KotlinVersion.KOTLIN_2_1
-val kotlinApiVersion = KotlinVersion.KOTLIN_2_1
 
 tasks.withType<KotlinCompilationTask<*>>().configureEach {
     compilerOptions {
@@ -40,8 +58,13 @@ tasks.withType<KotlinJvmCompile>().configureEach {
 configurations.all {
     resolutionStrategy {
         eachDependency {
+            /*
+             Manually align Kotlin version with the one defined in the Kotlin BOM without leaking strict version
+             constraints consumers.
+             */
             if (requested.group == "org.jetbrains.kotlin") {
-                useVersion("2.1.21")
+                useVersion(requireNotNull(libs.kotlin.bom.get().version))
+                because("Kotlin dependencies should use version from the Kotlin BOM")
             }
         }
     }
