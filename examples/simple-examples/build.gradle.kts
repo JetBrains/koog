56749a85fa9511c784246026654c0f481091b0fd
Andrey Bragin <andrey.bragin@jetbrains.com>
2025-09-23 09:51:16 +0200

    Clean up build.gradle.kts

--- examples/simple-examples/build.gradle.kts
+++ examples/simple-examples/build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
-    application
     id("ai.koog.gradle.plugins.credentialsresolver")
 }
-
-application.mainClass.set("ai.koog.example.composite_build_demo.MainKt")
 
 dependencies {
     implementation(platform(libs.kotlin.bom))
