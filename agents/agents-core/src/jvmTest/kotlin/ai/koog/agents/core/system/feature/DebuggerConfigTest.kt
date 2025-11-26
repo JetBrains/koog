9f451192d39384a1944783ab17e30226c30dac24
Mark.Tkachenko <mark.tkachenko@jetbrains.com>
2025-11-26 14:01:13 +0100

    mute DebuggerConfigTest

--- agents/agents-core/src/jvmTest/kotlin/ai/koog/agents/core/system/feature/DebuggerConfigTest.kt
+++ agents/agents-core/src/jvmTest/kotlin/ai/koog/agents/core/system/feature/DebuggerConfigTest.kt
@@ -38,6 +38,7 @@ import kotlin.time.toDuration
 // Isolate the environment by @Isolated annotation for these tests and make sure they are running without the parallelism.
 @Isolated
 @Execution(ExecutionMode.SAME_THREAD)
+@Disabled
 class DebuggerConfigTest {
 
     @AfterEach
