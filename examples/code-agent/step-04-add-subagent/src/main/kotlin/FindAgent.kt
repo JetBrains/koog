5753ef87b8c98ccc8f2858188c93a5c402952370
Bruno Lannoo <BLannoo@users.noreply.github.com>
2025-12-03 23:54:22 +0100

    :alembic: switch find agent back to GPT5Codex

--- examples/code-agent/step-04-add-subagent/src/main/kotlin/FindAgent.kt
+++ examples/code-agent/step-04-add-subagent/src/main/kotlin/FindAgent.kt
@@ -33,7 +33,7 @@ val findAgent = AIAgent(
         
         Ensure to utilize maximum amount of parallelization during the tool calling.
         """.trimIndent(),
-    llmModel = OpenAIModels.Chat.GPT4oMini,
+    llmModel = OpenAIModels.Chat.GPT5Codex,
     toolRegistry = ToolRegistry {
         tool(ListDirectoryTool(JVMFileSystemProvider.ReadOnly))
         tool(ReadFileTool(JVMFileSystemProvider.ReadOnly))
