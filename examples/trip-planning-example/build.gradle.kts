18b2170cb6eb42e9438f628aaed391f32738b95f
JochenGuck <6561457+JGuck@users.noreply.github.com>
2026-02-24 09:20:00 +0100

    Fix planning

--- examples/trip-planning-example/build.gradle.kts
+++ examples/trip-planning-example/build.gradle.kts
@@ -12,7 +12,6 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.kotlinx.datetime)
 
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.cio)
