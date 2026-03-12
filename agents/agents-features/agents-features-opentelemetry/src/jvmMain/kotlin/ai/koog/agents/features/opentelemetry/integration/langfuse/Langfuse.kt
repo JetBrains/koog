871bb5e41f34c821822293cf8a2ae215b91f6d24
Vadim Briliantov <vadim.briliantov@jetbrains.com>
2026-03-13 00:50:50 +0100

    Make Weave and Langfuse integrations available from Java

--- agents/agents-features/agents-features-opentelemetry/src/jvmMain/kotlin/ai/koog/agents/features/opentelemetry/integration/langfuse/Langfuse.kt
+++ agents/agents-features/agents-features-opentelemetry/src/jvmMain/kotlin/ai/koog/agents/features/opentelemetry/integration/langfuse/Langfuse.kt
@@ -1,5 +1,6 @@
 package ai.koog.agents.features.opentelemetry.integration.langfuse
 
+import ai.koog.agents.annotations.JavaAPI
 import ai.koog.agents.features.opentelemetry.attribute.CustomAttribute
 import ai.koog.agents.features.opentelemetry.feature.OpenTelemetryConfig
 import io.github.oshai.kotlinlogging.KotlinLogging
@@ -9,24 +10,8 @@ import java.util.concurrent.TimeUnit
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-/**
- * Configure an OpenTelemetry span exporter that sends data to [Langfuse](https://langfuse.com/).
- *
- * @param langfuseUrl the base URL of the Langfuse instance.
- *        If not a set is retrieved from `LANGFUSE_HOST` environment variable.
- *        Defaults to [https://cloud.langfuse.com](https://cloud.langfuse.com).
- * @param langfusePublicKey if not set is retrieved from `LANGFUSE_PUBLIC_KEY` environment variable.
- * @param langfuseSecretKey if not set is retrieved from `LANGFUSE_SECRET_KEY` environment variable.
- * @param timeout OpenTelemetry SpanExporter timeout.
- *        See [io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporterBuilder.setTimeout].
- * @param traceAttributes list of trace-level Langfuse attributes.
- *        See the full list: [Trace-Level Attributes](https://langfuse.com/integrations/native/opentelemetry#trace-level-attributes)
- *
- * @see <a href="https://langfuse.com/docs/get-started#create-new-project-in-langfuse">How to create a new project in Langfuse</a>
- * @see <a href="https://langfuse.com/faq/all/where-are-langfuse-api-keys">How to set up API keys in Langfuse</a>
- * @see <a href="https://langfuse.com/docs/opentelemetry/get-started#opentelemetry-endpoint">Langfuse OpenTelemetry Docs</a>
- */
-public fun OpenTelemetryConfig.addLangfuseExporter(
+
+internal fun OpenTelemetryConfig.addLangfuseExporterImpl(
     langfuseUrl: String? = null,
     langfusePublicKey: String? = null,
     langfuseSecretKey: String? = null,
