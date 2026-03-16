21c3bbc4bd05d025dd6618f4e367c1a4271def0e
antoniibelyshev <antoniy.belishev@gmail.com>
2026-03-16 12:28:39 +0100

    refactor ActionBuilder

--- agents/agents-core/src/commonMain/kotlin/ai/koog/agents/planner/goap/ActionBuilder.kt
+++ agents/agents-core/src/commonMain/kotlin/ai/koog/agents/planner/goap/ActionBuilder.kt
@@ -5,12 +5,6 @@ package ai.koog.agents.planner.goap
 /**
  * Builder for [Action] instances.
  */
-public expect class ActionBuilder<State>() : ActionBuilderApi<State> {
-    public override fun name(name: String): ActionBuilder<State>
-    public override fun description(description: String?): ActionBuilder<State>
-    public override fun precondition(precondition: Condition<State>): ActionBuilder<State>
-    public override fun belief(belief: Belief<State>): ActionBuilder<State>
-    public override fun cost(cost: Cost<State>): ActionBuilder<State>
-    public override fun execute(execute: Execute<State>): ActionBuilder<State>
-    public override fun build(): Action<State>
+public expect class ActionBuilder<State>() : ActionBuilderCommon<State, ActionBuilder<State>> {
+    public override fun self(): ActionBuilder<State>
 }
