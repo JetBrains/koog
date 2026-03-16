9b00635559233ac3617042848faae1e647cb6a96
antoniibelyshev <antoniy.belishev@gmail.com>
2026-03-16 15:41:52 +0100

    minor refactorings to the system variables reader

--- agents/agents-core/src/nonJvmCommonMain/kotlin/ai/koog/agents/core/system/SystemVariablesReader.kt
+++ agents/agents-core/src/nonJvmCommonMain/kotlin/ai/koog/agents/core/system/SystemVariablesReader.kt
@@ -3,13 +3,13 @@ package ai.koog.agents.core.system
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 internal actual object SystemVariablesReader {
 
-    // TODO: Add support for JS platform
+    // TODO: Add support for non-jvm platforms
     internal actual fun getEnvironmentVariable(name: String): String? {
-        throw NotImplementedError("Environment variables are not yet supported on JS platform")
+        throw NotImplementedError("Environment variables are not yet supported on non-jvm platforms")
     }
 
-    // TODO: Add support for JS platform
+    // TODO: Add support for non-jvm platforms
     internal actual fun getVMOption(name: String): String? {
-        throw NotImplementedError("VM Options are not supported on JS platform")
+        throw NotImplementedError("VM Options are not yet supported on non-jvm platforms")
     }
 }
