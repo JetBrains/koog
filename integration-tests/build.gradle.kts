5e8d977a8b57196fdc06e675d072c63349edb0a1
Alain-Michel Chomnoue Nghemning <a.chomnouenghemning@interpol.int>
2026-07-20 11:34:27 +0200

    Add integration tests and documentation

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -50,6 +50,7 @@ kotlin {
                 implementation(project(":agents:agents-features:agents-features-chat-history-aws"))
                 implementation(project(":agents:agents-features:agents-features-longterm-memory-aws"))
                 implementation(project(":serialization:serialization-jackson"))
+                implementation(project(":skills"))
 
                 // External libraries
                 implementation(libs.junit.jupiter.params)
